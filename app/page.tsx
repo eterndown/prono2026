82a8d7947bcd005bc40b91cb5205ca58731fe706
Pedro Pal <eterndown@gmail.com>
2026-03-04 01:07:56 +0000

    feat: sincronizar proyecto completo desde PC y limpiar archivos inútiles

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import App from "@/src/App";
 
 export default function Home() {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.push('/preview')
-  }, [router])
-
-  return null
+  return <App />;
 }
